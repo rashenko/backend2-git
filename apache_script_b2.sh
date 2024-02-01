893d5b1b5f846380af1dab65ced571ee06037ca3
rashenko <149801050+rashenko@users.noreply.github.com>
2024-02-01 18:05:47 +0300

    apache_script_b2.sh

--- apache_script_b2.sh
+++ apache_script_b2.sh
@@ -3,7 +3,7 @@
 DIR='/etc/apache2/sites-available/'
 DIR1='/home/qqee/backend2-git/000-backend2.conf'
 DIR2='/etc/apache2/sites-available/000-backend2.conf'
-DIR4='/var/www/html/index.html'
+DIR4='/home/qqee/backend2-git/index.html'
 DIR5='/var/www/backend2/'
 DIR6='/var/www/backend2/index.html'
 cp $DIR1 $DIR2
